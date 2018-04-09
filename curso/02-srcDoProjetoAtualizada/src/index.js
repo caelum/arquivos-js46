a4211fe3b3c9f058196afe98c82af3edd69ce4aa
Mario Souto <soutomarios@gmail.com>
2018-04-09 15:17:25 -0300

    [Update]

--- curso/02-srcDoProjetoAtualizada/src/index.js
+++ curso/02-srcDoProjetoAtualizada/src/index.js
@@ -7,6 +7,7 @@ import './assets/css/container.css'
 import './assets/css/btn.css'
 import './assets/css/icon.css'
 import './assets/css/iconHeart.css'
+import './assets/css/notificacao.css'
 
 import './assets/css/novoTweet.css'
 // import './index.css';
