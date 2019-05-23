9a1c64a497a6feb49153bfea6422190a8b9d6e19
Mario Souto <soutomarios@gmail.com>
2019-05-23 13:31:08 -0300

    Update index.js

--- curso/02-srcDoProjetoAtualizada/src/components/NavMenu/index.js
+++ curso/02-srcDoProjetoAtualizada/src/components/NavMenu/index.js
@@ -1,30 +1,34 @@
-import React, { Component } from 'react'
-import './navMenu.css'
+import React, { Component } from "react";
+import navMenuStyles from "./navMenu.module.css";
 
-class NavMenu extends Component {
-    render() {
-        return (
-            <nav className="navMenu">
-                <ul className="navMenu__lista">
-                <li className="navMenu__item">
-                    <a className="navMenu__link" href="/">
-                        Bem vindo(a): <br />
-                        <strong>{ this.props.usuario }</strong>
-                    </a>
-                </li>
-                <li className="navMenu__item">
-                    <a className="navMenu__link" href="/">Página Inicial</a>
-                </li>
-                <li className="navMenu__item">
-                    <a className="navMenu__link" href="/">Hashtags</a>
-                </li>
-                <li className="navMenu__item">
-                    <a className="navMenu__link" href="/">Logout</a>
-                </li>
-                </ul>
-            </nav>
-        )
-    }
+export default class NavMenu extends Component {
+  render() {
+    return (
+      <nav className={navMenuStyles.navMenu}>
+        <ul className={navMenuStyles.navMenu__lista}>
+          <li className={navMenuStyles.navMenu__item}>
+            <a className={navMenuStyles.navMenu__link} href="/">
+              Bem vindo(a): <br />
+              <strong>{this.props.usuario}</strong>
+            </a>
+          </li>
+          <li className={navMenuStyles.navMenu__item}>
+            <a className={navMenuStyles.navMenu__link} href="/">
+              Página Inicial
+            </a>
+          </li>
+          <li className={navMenuStyles.navMenu__item}>
+            <a className={navMenuStyles.navMenu__link} href="/hashtags">
+              Hashtags
+            </a>
+          </li>
+          <li className={navMenuStyles.navMenu__item}>
+            <a className={navMenuStyles.navMenu__link} href="/logout">
+              Logout
+            </a>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
 }
-
-export default NavMenu
