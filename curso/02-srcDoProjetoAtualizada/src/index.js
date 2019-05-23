a5d62bbdf8cf6c1f6ce9ff24c09551ed025ceaa7
Mario Souto <soutomarios@gmail.com>
2019-05-23 13:40:32 -0300

    Update index.js

--- curso/02-srcDoProjetoAtualizada/src/index.js
+++ curso/02-srcDoProjetoAtualizada/src/index.js
@@ -14,7 +14,7 @@ import './assets/css/novoTweet.css'
 
 
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
